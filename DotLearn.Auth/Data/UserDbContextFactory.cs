ef43c55d3805d9c14c2fe5a0517435e4494f8c0d
SAMARTH SHAKYA <122541726+SAMARTH-SHAKYA@users.noreply.github.com>
2026-04-06 13:50:19 +0530

    chore(infra): add AWS Secrets Manager environment guard and clean .gitignore

--- DotLearn.Auth/Data/UserDbContextFactory.cs
+++ DotLearn.Auth/Data/UserDbContextFactory.cs
@@ -8,7 +8,8 @@ public class UserDbContextFactory : IDesignTimeDbContextFactory<UserDbContext>
     public UserDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<UserDbContext>();
-        optionsBuilder.UseSqlServer("Server=placeholder;Database=UserDb;");
+        var connStr = "Server=dotlearn-db.c7ge68ueyfep.ap-southeast-2.rds.amazonaws.com,1433;Database=AuthDb;User Id=admin;Password=DOTLearn@123;TrustServerCertificate=True";
+        optionsBuilder.UseSqlServer(connStr);
         return new UserDbContext(optionsBuilder.Options);
     }
 }
